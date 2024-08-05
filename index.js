005b36bc8203da3316e80f9564ab5b33ea6bbb21
Riikka-KaoPT23TIEd <rixuuu95@gmail.com>
2024-08-05 11:38:34 +0300

    Added two projects and tried to make modal window for the projects images, so they can be opened 'fullscreen' version. It works, but could be better, so still in progress I guess.

--- index.js
+++ index.js
@@ -78,15 +78,53 @@ document.addEventListener('DOMContentLoaded', function()
         }
 
 
+        //Modal functionality
+        const modal = document.getElementById("imageModal");
+        const modalImg = document.getElementById("modal-img");
+        let currentModalProjectId = 0;
+
+        function openModal(projectId) {
+            currentModalProjectId = projectId;
+            modal.style.display = "block";
+            showModalImage();
+        }
+
+        function closeModal() {
+            modal.style.display = "none";
+        }
+
+        function showModalImage() {
+            modalImg.src = imageList[currentModalProjectId][currentImageIndex[currentModalProjectId]];
+        }
+
+        function modalNextImage() {
+            currentImageIndex[currentModalProjectId] = (currentImageIndex[currentModalProjectId] + 1) % imageList[currentModalProjectId].length;
+            showModalImage();
+        }
+
+        function modalPreviousImage() {
+            currentImageIndex[currentModalProjectId] = (currentImageIndex[currentModalProjectId] - 1 + imageList[currentModalProjectId].length) % imageList[currentModalProjectId].length;
+            showModalImage();
+        }
+
+        window.openModal = openModal;
+        window.closeModal = closeModal;
+        window.modalNextImage = modalNextImage;
+        window.modalPreviousImage = modalPreviousImage;
+
         //Here is functionality for the portfolio images
         const imageList = 
         {
-            1: ["Images/tervetuloaph.png", "Images/escaperoomPuzzle1.png", "Images/escroomFirstpuzzle.png", "Images/puzzlehint.png", "Images/gratzEscaped.png", "Images/adminpassword.png", "Images/admintoolspage.png", "Images/lisaapakohuone.png"]
+            1: ["Images/tervetuloaph.png", "Images/escaperoomPuzzle1.png", "Images/escroomFirstpuzzle.png", "Images/puzzlehint.png", "Images/gratzEscaped.png", "Images/adminpassword.png", "Images/admintoolspage.png", "Images/lisaapakohuone.png"],
+            2: ["Images/phonebookMainpage.png", "Images/phonebookFiltersearch.png", "Images/phonebookReplace.png", "Images/pbPersonDelete.png", "Images/phonebookserverdata.png", "Images/pbPersonDeleteSuccess.png", "Images/pbServerPersondeleteSuccess.png"],
+            3: ["Images/country-information.png", "Images/country-info-error.png", "Images/country-info-search.png", "Images/country-info-details.png"]
         }
 
         const currentImageIndex =
         {
-            1: 0
+            1: 0,
+            2: 0,
+            3: 0
         }
 
         function showImage(projectId)
